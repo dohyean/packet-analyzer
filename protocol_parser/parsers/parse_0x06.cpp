84b62ed279898bc7b27676a36bc17b7f9ae95023
dohyeon <gkscjsegus@naver.com>
2025-10-01 19:13:21 +0900

    Add parsers/parse_0x06.cpp - add request, response parser

---
+++ protocol_parser/parsers/parse_0x06.cpp
@@ -0,0 +1,44 @@
+#include "ModbusHeader.h"
+
+std::vector<std::pair<std::string,std::string>>
+ModbusHeader::parse_0x06(const std::vector<uint8_t>& frame, int pdu_start, int pdu_end, const std::string type){
+    std::vector<std::pair<std::string,std::string>> fields;
+    fields.push_back({"function_code", "0x06"});
+    fields.push_back({"function_name", "Write Single Register"});
+
+    if(type == "request"){
+        // request PDU 유효성 검사
+        if (frame.size() - (size_t)(pdu_start) < 5) {
+            throw std::runtime_error("Invalid Modbus 0x06 request: PDU too short");
+        }
+        if(frame.size() - (size_t)pdu_start > 5) {
+            throw std::runtime_error("Invalid Modbus 0x06 request: PDU too long");
+        }
+
+        // request output_address, output_value 추출
+        uint16_t register_addr = (frame[pdu_start+1] << 8) | frame[pdu_start+2];
+        uint16_t register_value = (frame[pdu_start+3] << 8) | frame[pdu_start+4];
+    
+        // request 필드 추가
+        fields.push_back({"Register Address", std::to_string(register_addr)});
+        fields.push_back({"Register Value", std::to_string(register_value)});
+    }
+    else if(type == "response"){
+        // response PDU 유효성 검사
+        if (frame.size() - (size_t)pdu_start < 5) {
+            throw std::runtime_error("Invalid Modbus 0x06 response: PDU too short");
+        }
+        if (frame.size() - (size_t)pdu_start > 5) {
+            throw std::runtime_error("Invalid Modbus 0x06 response: PDU too long");
+        }
+
+        // response output_address, output_value 추출
+        uint16_t register_addr = (frame[pdu_start+1] << 8) | frame[pdu_start+2];
+        uint16_t register_value = (frame[pdu_start+3] << 8) | frame[pdu_start+4];
+
+        // response 필드 추가
+        fields.push_back({"Register Address", std::to_string(register_addr)});
+        fields.push_back({"Register Value", std::to_string(register_value)});
+    }
+    return fields;
+}
