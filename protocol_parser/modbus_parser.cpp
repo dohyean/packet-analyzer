f433fb2d7571c83c771701f4c073f3f6adad8e95
dohyeon <gkscjsegus@naver.com>
2025-10-01 16:41:25 +0900

    Modify modbus_parser.cpp - remove 0x04 parser code, add run_modbus_process_test for 0x04

--- protocol_parser/modbus_parser.cpp
+++ protocol_parser/modbus_parser.cpp
@@ -182,47 +182,10 @@ void run_modbus_process_test(const std::vector<std::string>& frames,
 }
 
 int main() {
-    // int number = 2;
-    // Modbus/TCP Request, Response, Error frames
-    // vector<string> req_data = {
-    //     "02 42 b9 af 00 05 02 42 b9 af 00 03 08 00 45 00 00 40 03 6d 40 00 40 06 c3 e4 b9 af 00 03 b9 af 00 05 e6 54 01 f6 a6 d0 cc 60 41 89 89 a6 80 18 01 f6 73 99 00 00 01 01 08 0a 84 5b 99 77 32 eb 86 7f 00 03 00 00 00 06 01 01 00 19 00 01", // 0x01
-    //     "02 42 b9 af 00 05 02 42 b9 af 00 03 08 00 45 00 00 40 66 7e 40 00 40 06 60 d3 b9 af 00 03 b9 af 00 05 e6 5a 01 f6 9c 95 77 8a 9a ce 61 dd 80 18 01 f6 73 99 00 00 01 01 08 0a 84 5b 99 a1 32 eb 86 aa 00 05 00 00 00 06 01 02 00 05 00 01", // 0X02
-    //     "0242b9af00050242b9af0003080045000040d59940004006f1b7b9af0003b9af0005e65001f69f549c44247c6f42801801f6739900000101080a845b994b32eb8654000100000006010300230001", // 0x03
-
-    //     "02 42 b9 af 00 05 02 42 b9 af 00 03 08 00 45 00 00 40 d5 99 40 00 40 06 f1 b7 b9 af 00 03 b9 af 00 05 e6 50 01 f6 9f 54 9c 44 24 7c 6f 42 80 18 01 f6 73 99 00 00 01 01 08 0a 84 5b 99 4b 32 eb 86 54 00 01 00 00 00 06 01 04 00 23 00 02", // 0x04
-    // };
-    // vector<uint8_t> req_frame = hex_to_bytes(req_data[number]);
-    
-    // vector<string> res_data = {
-    //     "02 42 b9 af 00 03 02 42 b9 af 00 05 08 00 45 00 00 3e 5b be 40 00 40 06 6b 95 b9 af 00 05 b9 af 00 03 01 f6 e6 54 41 89 89 a6 a6 d0 cc 6c 80 18 01 fd 73 97 00 00 01 01 08 0a 32 eb 86 80 84 5b 99 77 00 03 00 00 00 04 01 01 01 00", // 0x01
-    //     "02 42 b9 af 00 03 02 42 b9 af 00 05 08 00 45 00 00 3e 6d 4f 40 00 40 06 5a 04 b9 af 00 05 b9 af 00 03 01 f6 e6 5a 9a ce 61 dd 9c 95 77 96 80 18 01 fd 73 97 00 00 01 01 08 0a 32 eb 86 ab 84 5b 99 a1 00 05 00 00 00 04 01 02 01 00", // 0X02
-    //     "0242b9af00030242b9af000508004500003f56d440004006707eb9af0005b9af000301f6e650247c6f429f549c50801801fd739800000101080a32eb8654845b994b000100000005010303000900010019", // 0x03
-
-    //     "02 42 b9 af 00 05 02 42 b9 af 00 03 08 00 45 00 00 40 d5 99 40 00 40 06 f1 b7 b9 af 00 05 b9 af 00 03 e6 50 01 f6 9f 54 9c 44 24 7c 6f 42 80 18 01 f6 73 99 00 00 01 01 08 0a 84 5b 99 4b 32 eb 86 54 00 01 00 00 00 07 01 04 04 11 11 22 22", // 0x04
-    // };
-    // vector<uint8_t> res_frame = hex_to_bytes(res_data[number]);
-
-    // vector<string> err_data = {
-    //     "02 42 b9 af 00 05 02 42 b9 af 00 03 08 00 45 00 00 40 d5 99 40 00 40 06 f1 b7 b9 af 00 03 b9 af 00 05 e6 50 01 f6 9f 54 9c 44 24 7c 6f 42 80 18 01 f6 73 99 00 00 01 01 08 0a 84 5b 99 4b 32 eb 86 54 00 01 00 00 00 03 01 81 01", // exception 0x01
-    //     "02 42 b9 af 00 05 02 42 b9 af 00 03 08 00 45 00 00 40 d5 99 40 00 40 06 f1 b7 b9 af 00 03 b9 af 00 05 e6 50 01 f6 9f 54 9c 44 24 7c 6f 42 80 18 01 f6 73 99 00 00 01 01 08 0a 84 5b 99 4b 32 eb 86 54 00 01 00 00 00 03 01 82 02", // exception 0x02
-    //     "02 42 b9 af 00 05 02 42 b9 af 00 03 08 00 45 00 00 40 d5 99 40 00 40 06 f1 b7 b9 af 00 03 b9 af 00 05 e6 50 01 f6 9f 54 9c 44 24 7c 6f 42 80 18 01 f6 73 99 00 00 01 01 08 0a 84 5b 99 4b 32 eb 86 54 00 01 00 00 00 03 01 83 03", // exception 0x03
-    
-    
-    //     "02 42 b9 af 00 05 02 42 b9 af 00 03 08 00 45 00 00 40 d5 99 40 00 40 06 f1 b7 b9 af 00 03 b9 af 00 05 e6 50 01 f6 9f 54 9c 44 24 7c 6f 42 80 18 01 f6 73 99 00 00 01 01 08 0a 84 5b 99 4b 32 eb 86 54 00 01 00 00 00 03 01 84 04", // exception 0x04
-    // };
-    // vector<uint8_t> err_frame = hex_to_bytes(err_data[number]);
-
-    // string SCADA_IP = "185.175.0.3";
-
-    // process_frame(req_frame);
-    // process_frame(res_frame);
-    // process_frame(err_frame);
-
-    // json j = process_frame_json(req_frame);
-    // std::cout << j.dump(4) << std::endl;
-
-
-    run_modbus_process_test(modbus_0x03_testing, expected_0x03, "0x03");
+    // run_modbus_process_test(modbus_0x01_testing, expected_0x01, "0x01");
+    // run_modbus_process_test(modbus_0x02_testing, expected_0x02, "0x02");
+    // run_modbus_process_test(modbus_0x03_testing, expected_0x03, "0x03");
+    run_modbus_process_test(modbus_0x04_testing, expected_0x04, "0x04");
 
     // cout << "========================================\n최종 실행 단계: Modbus PDU 파싱\n";
     return 0;
